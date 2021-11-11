df44189d65833df87872f0536f888e203292a7fc
pntehan <2390003207@qq.com>
2021-11-11 17:16:56 +0800

    version1

---
+++ src/network.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# File    :   network.py
+# Time    :   2021/11/11 17:13:06
+# Author  :   Pnte Han 
+# Version :   1.0
+# Contact :   pntehan@gmail.com
+# Desc    :   None
+
+
+import tensorflow as tf
+
+
+
